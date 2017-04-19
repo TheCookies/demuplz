e4f372e2bd9f8be9b25a4ced0291cc527a753fd6
TheCookies <tyson@tysonclark.com>
2017-04-19 17:16:35 -0400

    Original Upload

---
+++ app/settings.php
@@ -0,0 +1,9 @@
+<?php
+$settings['db_host'] = 'localhost';
+$settings['db_name'] = 'uploader';
+$settings['db_user'] = 'uploader';
+$settings['db_pass'] = 'a1s2f15s4f5s5f4s5s4ff';
+$settings['db_table_prefix'] = 'chv_';
+$settings['db_driver'] = 'mysql';
+$settings['db_pdo_attrs'] = [];
+$settings['debug_level'] = 1;
